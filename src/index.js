a39791dd19d60821e2f40c4a35b2f7ab6a1d0c5c
danielthetam <danieltzytzytzy@gmail.com>
2023-07-17 22:05:51 +0800

    initial push reactview

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+import Form from './form.js'
+import AdminView from './admin.js'
 
+function Route() {
+  if (window.location.pathname === '/') {
+    return React.createElement(Form);
+  } else if (window.location.pathname === '/admin') {
+    return React.createElement(AdminView);
+  } else {
+    window.location.pathname = '/';
+    return React.createElement(Form);
+  }
+}
+
+function App() {
+  return (
+    <Route />
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />    
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
